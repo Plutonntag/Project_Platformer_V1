e36f5efe8eb5e16e2c8707441ec584fca89a72b0
Plutonntag <ilian.hamrouni@laposte.net>
2024-03-30 14:58:18 +0100

    Ajout_ennemi_et_shadow_caster

---
+++ Assets/Move_ennemie.cs
@@ -0,0 +1,41 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Rendering;
+using UnityEngine;
+
+public class Move_ennemie : MonoBehaviour
+{
+    [SerializeField] float Speed;
+    [SerializeField] Transform[] Waypoint;
+    private Transform target;
+    private int desPoint = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        target = Waypoint[0];
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Basic_movement();
+
+    }
+
+    private void Basic_movement()
+    {
+        Vector3 dir = target.position - transform.position;     //Donne la direction
+        transform.Translate(dir.normalized * Speed * Time.deltaTime, Space.World);  //Donne le mouvement
+
+
+        if (Vector3.Distance(transform.position, target.position) < 0.3f)    // Si pas trop loin du target, on change.
+        {
+            desPoint = (desPoint + 1) % Waypoint.Length;   // Utilisation du reste pour switché entre le point 1 et 2
+            target = Waypoint[desPoint];                   // Change de target
+
+        }
+
+
+    }
+}
