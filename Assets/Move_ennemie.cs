3fe225b42f50e2a03156d46dd769bb89001992d6
Plutonntag <ilian.hamrouni@laposte.net>
2024-03-30 15:52:57 +0100

    Ennemi_hit

--- Assets/Move_ennemie.cs
+++ Assets/Move_ennemie.cs
@@ -7,19 +7,29 @@ public class Move_ennemie : MonoBehaviour
 {
     [SerializeField] float Speed;
     [SerializeField] Transform[] Waypoint;
+    [SerializeField] SpriteRenderer Graph;
+    [SerializeField] BoxCollider2D triggHit;
+
     private Transform target;
     private int desPoint = 0;
+    [SerializeField] bool canHit;
+    IEnumerator coroutine;
 
     // Start is called before the first frame update
     void Start()
     {
         target = Waypoint[0];
+        canHit = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Basic_movement();
+        if (!canHit)
+        {
+            Basic_movement();
+
+        }
 
     }
 
@@ -29,13 +39,48 @@ public class Move_ennemie : MonoBehaviour
         transform.Translate(dir.normalized * Speed * Time.deltaTime, Space.World);  //Donne le mouvement
 
 
+
         if (Vector3.Distance(transform.position, target.position) < 0.3f)    // Si pas trop loin du target, on change.
         {
             desPoint = (desPoint + 1) % Waypoint.Length;   // Utilisation du reste pour switché entre le point 1 et 2
             target = Waypoint[desPoint];                   // Change de target
+            Graph.flipX = !Graph.flipX;
+            triggHit.offset = new Vector3(-triggHit.offset.x, triggHit.offset.y);
 
         }
 
 
     }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canHit = true;
+            StartCoroutine(Hit());
+
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.CompareTag("Player"))
+        {
+            canHit = false;
+            StopCoroutine(Hit());
+
+        }
+    }
+
+
+    IEnumerator Hit()
+    {
+        while (canHit)
+        {
+            yield return new WaitForSeconds(2);
+            Debug.Log("Hit");
+        }
+
+    }
+    
 }
